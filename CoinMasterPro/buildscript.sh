467b6fc11a1131a157e0228834d9e2320e50b397
Elias <eliasnemr@gmail.com>
2021-05-14 13:05:34 +0300

    Init Minima

---
+++ CoinMasterPro/buildscript.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -rf ../minidapp
+
+mkdir ../minidapp
+
+mkdir ../minidapp/CoinMasterPro
+
+cp -rf www/* ../minidapp/CoinMasterPro
+
+cp src/assets/bg.svg ../minidapp/CoinMasterPro
+
+cp src/assets/minidapp.conf.orig minidapp/CoinMasterPro/minidapp.conf
+
+zip -r ../minidapp/coinmasterpro.minidapp ../minidapp/CoinMasterPro
+
+
