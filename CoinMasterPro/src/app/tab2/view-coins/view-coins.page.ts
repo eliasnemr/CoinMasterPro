17feba36d10365019adbf7a249144f28a41f7afe
Elias <eliasnemr@gmail.com>
2021-05-16 03:17:29 +0300

    Form, form data init, checkboxes connected to coinArr, submit ready

--- CoinMasterPro/src/app/tab2/view-coins/view-coins.page.ts
+++ CoinMasterPro/src/app/tab2/view-coins/view-coins.page.ts
@@ -2,8 +2,9 @@ import { ToolsService } from './../../services/tools.service';
 import { ApiService } from './../../services/api.service';
 import { Token } from 'minima';
 import { Subscription } from 'rxjs';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 interface TokenId {
   tokenId: string
@@ -38,20 +39,26 @@ export class ViewCoinsPage {
   found: boolean;
   isCoinsFound: boolean;
   tokenSelectedId: TokenId;
+  aggregateForm: FormGroup;
   
   constructor(
     private route: ActivatedRoute, 
     private api: ApiService,
-    private tools: ToolsService) {
+    private tools: ToolsService,
+    private formBuilder: FormBuilder) {
+    /** initialize form */
+    this.initForm();
+    /** subscribe to router url params */
     this.$routerSubscription =
     this.route.params.subscribe((res: TokenId) => {
+      /** subscribe to balance observable */
       this.$balanceSubscription =
       this.api.$balance.subscribe((tokens: Token[]) => {
         this.token = tokens.filter(token => token.tokenid === res.tokenId);
-
+        /** check if found any token matching after filtering */
         if (this.token.length > 0) {
           this.found = true;
-
+          /** get coins for selected token */
           const coins: any =
           this.api.getCoinsForToken(this.token[0].tokenid);
           try {
@@ -59,9 +66,11 @@ export class ViewCoinsPage {
               coins.then((res: any) => {
                 const totalCoins = res.response.coins;
                 this.coins = totalCoins;
+                /** if coins found return true.. */
                 if (this.coins.length > 0) {
                   this.isCoinsFound = true;
                   console.log(this.coins);
+                /** else.. */
                 } else {
                   this.isCoinsFound = false;
                   console.log('No coins found.');
@@ -72,7 +81,7 @@ export class ViewCoinsPage {
           } catch (err) {
             throw new Error(cryptocurrency + ' : failed to fetch coins.');
           }
-
+          /** create avatar for tokens if no provided icon */
           if (this.token[0].tokenid !== '0x00' && this.token[0].icon === '') {
             this.token[0].icon = this.tools.createAvatarIcon(this.token[0].tokenid);
           }
@@ -94,5 +103,37 @@ export class ViewCoinsPage {
   ionViewWillLeave() {
     this.$routerSubscription.unsubscribe();
     this.$balanceSubscription.unsubscribe();
+  }
+
+  initForm() {
+    this.aggregateForm = this.formBuilder.group({
+      selectedCoinsArr: this.formBuilder.array([], [Validators.required])
+    });
+  }
+
+  resetForm() {
+    this.aggregateForm.reset();
+    this.initForm();
+  }
+
+  onCheckboxChange(e) {
+    const checkArray: FormArray = this.aggregateForm.get('selectedCoinsArr') as FormArray;
+
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      checkArray.controls.forEach((item: FormControl) => {
+        if (item.value == e.target.value) {
+          checkArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
+  }
+
+  submitForm() {
+    
   }
 }
